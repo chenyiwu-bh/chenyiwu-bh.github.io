10badf0c7692a967a041df4652f290461c7f15dd
chenyiwu-bh <1457483328@qq.com>
2024-08-02 23:52:22 +0800

    Site updated: 2024-08-02 23:52:19

---
+++ js/duration.js
@@ -0,0 +1,56 @@
+!(function() {
+  function duration() {
+    var start = new Date("2024/07/05 12:00:00");
+    var now = new Date();
+    now.setTime(now.getTime()+250);
+    days = (now - start) / 1000 / 60 / 60 / 24;
+    dnum = Math.floor(days);
+    hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+    hnum = Math.floor(hours);
+    if(String(hnum).length === 1 ){
+      hnum = "0" + hnum;
+    }
+    minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+    mnum = Math.floor(minutes);
+    if(String(mnum).length === 1 ){
+      mnum = "0" + mnum;
+    }
+    seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+    snum = Math.round(seconds);
+    if(String(snum).length === 1 ){
+      snum = "0" + snum;
+    }
+    document.getElementById("timeDate-from-setup").innerHTML = "本站已安全运行&nbsp"+dnum+"&nbsp天";
+    document.getElementById("times-from-setup").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    
+    var start = new Date("2024/08/01 15:31:08");
+    var now = new Date();
+    now.setTime(now.getTime()+250);
+    days = (now - start) / 1000 / 60 / 60 / 24;
+    dnum = Math.floor(days);
+    hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+    hnum = Math.floor(hours);
+    if(String(hnum).length === 1 ){
+      hnum = "0" + hnum;
+    }
+    minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+    mnum = Math.floor(minutes);
+    if(String(mnum).length === 1 ){
+      mnum = "0" + mnum;
+    }
+    seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+    snum = Math.round(seconds);
+    if(String(snum).length === 1 ){
+      snum = "0" + snum;
+    }
+    if(document.getElementById("timeDate-from-update") != null ){
+      document.getElementById("timeDate-from-update").innerHTML = "距离上次更新已过去&nbsp"+dnum+"&nbsp天";
+    }
+    if(document.getElementById("times-from-update") != null ){
+      document.getElementById("times-from-update").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    }
+  }
+
+  duration();
+  setInterval(duration, 1000);
+})();
